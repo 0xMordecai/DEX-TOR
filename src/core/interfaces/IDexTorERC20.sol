5b134f005b10023f1979901b55076e7a8a894cb0
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-04-08 23:17:24 +0100

    add IDexTorERC20 intrface for DexTorERC20

--- src/core/interfaces/IDexTorERC20.sol
+++ src/core/interfaces/IDexTorERC20.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+
+interface IDexTorERC20 {
+    function mint(address _to, uint256 _amount) external returns (bool);
+
+    function burn(uint256 _amount) external;
+
+    function getTotalSupply() external view returns (uint256);
+
+    function getBalanceOf(address _account) external view returns (uint256);
+}
--- test/core/DexTorERC20.t.sol
+++ test/core/DexTorERC20.t.sol
@@ -2,5 +2,6 @@
 pragma solidity 0.8.28;
 
 import {Test} from "forge-std/Test.sol";
+import {DexTorERC20} from "src/core/DexTorERC20.sol";
 
 contract DexTorERC20Test is Test {}
