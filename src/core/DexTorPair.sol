490fbf79bb93380b7f6d140bec9c25319ede200c
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-04-13 00:21:55 +0100

    adding _update private function in DexTorPair.sol

--- src/core/DexTorPair.sol
+++ src/core/DexTorPair.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+import {UQ112x112} from "src/core/libraries/UQ112x112.sol";
 
 contract DexTorPair {
+    using UQ112x112 for uint224;
     error DexTorPair__ZeroAddress();
     error DexTorPair__TokensHaveSameAddresses();
     error DexTorPair__BurnAmountExceedsBalance();
     error DexTorPair__LOCKED();
+    error DexTorPair__BalanceExceedsUint112Max();
 
     address public immutable factory;
     address public immutable token0;
     address public immutable token1;
 
-    uint256 private reserve0;
-    uint256 private reserve1;
+    uint112 private reserve0;
+    uint112 private reserve1;
     uint32 private blockTimestampLast;
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Burn(
@@ -30,7 +37,7 @@ contract DexTorPair {
         uint256 amount1Out,
         address indexed to
     );
-    event Sync(uint256 reserve0, uint256 reserve1);
+    event Sync(uint112 reserve0, uint112 reserve1);
 
     constructor(address _token0, address _token1) {
         if (_token0 == address(0) || _token0 == address(0)) {
@@ -58,13 +65,43 @@ contract DexTorPair {
         public
         view
         returns (
-            uint256 _reserve0,
-            uint256 _reserve1,
+            uint112 _reserve0,
+            uint112 _reserve1,
             uint32 _blockTimestampLast
         )
     {
         _reserve0 = reserve0;
         _reserve1 = reserve1;
         _blockTimestampLast = blockTimestampLast;
+    }
+
+    function _update(
+        uint balance0,
+        uint balance1,
+        uint112 _reserve0,
+        uint112 _reserve1
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max) {
+            revert DexTorPair__BalanceExceedsUint112Max();
+        }
+        uint32 blockTimestamp = uint32(block.timestamp % 2 ** 32);
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+        if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
+            unchecked {
+                // price of x in term of y
+                price0CumulativeLast +=
+                    uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) *
+                    timeElapsed;
+                // price of y in term of x
+                price1CumulativeLast +=
+                    uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) *
+                    timeElapsed;
+            }
+        }
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = blockTimestamp;
+
+        emit Sync(reserve0, reserve1);
     }
 }
