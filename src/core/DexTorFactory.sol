2c63b7190ea63955f151fccb63abe6e36ac09ed9
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-04-21 21:21:56 +0100

    completing DexTorFactory contract by completing createPair function

--- src/core/DexTorFactory.sol
+++ src/core/DexTorFactory.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+import {DexTorPair} from "src/core/DexTorPair.sol";
 import {IDexTorFactory} from "src/core/interfaces/IDexTorFactory.sol";
 
 contract DexTorFactory is IDexTorFactory {
     error DexTorFactory__NotFeeToSetter();
+    error DexTorFactory__SameTokenAddress();
+    error DexTorFactory__ZeroAddress();
+    error DexTorFactory__PairExists();
+
     address public feeTo;
     address public feeToSetter;
 
@@ -25,7 +30,30 @@ contract DexTorFactory is IDexTorFactory {
     function createPair(
         address tokenA,
         address tokenB
-    ) external returns (address pair) {}
+    ) external returns (address pair) {
+        if (tokenA == tokenB) {
+            revert DexTorFactory__SameTokenAddress();
+        }
+        (address token0, address token1) = tokenA < tokenB
+            ? (tokenA, tokenB)
+            : (tokenB, tokenA);
+        if (token0 == address(0)) {
+            revert DexTorFactory__ZeroAddress();
+        }
+        if (TokensPair[token0][token1] != address(0)) {
+            revert DexTorFactory__PairExists();
+        }
+        /**
+         * type(UniswapV2Pair).creationCode returns the initialization (creation) bytecode of the UniswapV2Pair contract.
+         */
+        bytes memory bytecode = type(DexTorPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        pair = address(new DexTorPair{salt: salt}(token0, token1));
+        TokensPair[token0][token1] = pair;
+        TokensPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
 
     function setFeeTo(address _feeTo) external {
         if (msg.sender != feeToSetter) {
